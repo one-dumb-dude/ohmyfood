7320fef902d47afe6239b87866e9da64fe5f72da
David Cotto <dacotto1984@gmail.com>
2023-05-28 10:41:04 -0400

    fixing loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,21 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Check if the current page is not the index page
-    if (!window.location.pathname.endsWith('index.html') || window.location.pathname !== '/') {
-        // Create the new div element
-        let newDiv = document.createElement("div");
+if (!window.location.pathname.endsWith('index.html') && window.location.pathname !== '/') {
 
-        // Style the new div
-        newDiv.style.position = "fixed";
-        newDiv.style.top = "0";
-        newDiv.style.left = "0";
-        newDiv.style.width = "100vw";
-        newDiv.style.height = "100vh";
-        newDiv.style.backgroundColor = "rgba(255, 255, 255, 0.5)"; // semi-transparent white
-        newDiv.style.zIndex = "1"; // You might need to adjust this based on your other elements
+    let newDiv = document.createElement("div");
 
-        // Append the new div to the body of the document
-        document.body.appendChild(newDiv);
+    newDiv.style.position = "fixed";
+    newDiv.style.top = "0";
+    newDiv.style.left = "0";
+    newDiv.style.width = "100vw";
+    newDiv.style.height = "100vh";
+    newDiv.style.backgroundColor = "rgba(255, 255, 255, .95)"; // semi-transparent white
+    newDiv.style.zIndex = "1";
 
-        // Set an animation end listener on the #loader element
-        document.querySelector('#loader').addEventListener('animationend', (e) => {
+    document.body.appendChild(newDiv);
+
+    const loader = document.querySelector('#loader');
+
+    if (loader) {
+        loader.addEventListener('animationend', (e) => {
             if (e.animationName === 'loading') {
                 // Remove the new div when the loader's animation ends
                 e.target.style.display = 'none';
@@ -25,4 +23,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
             }
         });
     }
-});
+}
+
+
