fcf91ff80d6ed00d9c7e5c4924839d22ed1d90a3
David Cotto <dacotto1984@gmail.com>
2023-05-28 10:17:14 -0400

    added a loader

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Check if the current page is not the index page
+if (!window.location.pathname.endsWith('index.html')) {
+    // Create the new div element
+    let newDiv = document.createElement("div");
+
+    // Style the new div
+    newDiv.style.position = "fixed";
+    newDiv.style.top = "0";
+    newDiv.style.left = "0";
+    newDiv.style.width = "100vw";
+    newDiv.style.height = "100vh";
+    newDiv.style.backgroundColor = "rgba(255, 255, 255, 0.5)"; // semi-transparent white
+    newDiv.style.zIndex = "1"; // You might need to adjust this based on your other elements
+
+    // Append the new div to the body of the document
+    document.body.appendChild(newDiv);
+
+    // Set an animation end listener on the #loader element
+    document.querySelector('#loader').addEventListener('animationend', (e) => {
+        if (e.animationName === 'loading') {
+            // Remove the new div when the loader's animation ends
+            e.target.style.display = 'none';
+            newDiv.remove();
+        }
+    });
+}
+
+
